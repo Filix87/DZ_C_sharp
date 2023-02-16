5beda394bc8fd8d3cb166e384809fe30babd9d81
GenaPC <lotog@yandex.ru>
2023-02-16 15:34:36 +0300

    решение задачи на ветке task2

--- Program.cs
+++ Program.cs
@@ -8,3 +8,20 @@
 22 3 9 -> 22
 */
 
+int number, number2, number3;
+
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out number);
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out number2);
+Console.Write("Введите третье число: ");
+int.TryParse(Console.ReadLine()!, out number3);
+
+if (number > number2 && number > number3)
+    Console.Write($"max: {number}");
+
+else if (number2 > number && number2 > number3)
+    Console.Write($"max: {number2}");
+
+else if (number3 > number && number3 > number2)
+    Console.Write($"max: {number3}");
