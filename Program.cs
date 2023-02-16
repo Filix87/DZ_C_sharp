5134b09c555bb97c63f7957b999a5d3707285ffb
GenaPC <lotog@yandex.ru>
2023-02-16 18:13:54 +0300

    решение задачи на ветке task4

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@
 8 -> 2, 4, 6, 8
 */
 
+int N;
+Console.Write($"Введите число: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+
+if (flag)
+{
+    for (int i = 2; i <= N; i += 2)
+        Console.Write($"{i}, ");
+}
+else
+    Console.Write("Неверный ввод");
