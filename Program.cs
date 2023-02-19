f3f753a6784b7b72eb7dacf7255925c3de62f93c
GenaPC <lotog@yandex.ru>
2023-02-19 14:03:15 +0300

    Решение задачи на ветке task7

--- Program.cs
+++ Program.cs
@@ -6,3 +6,13 @@
 1 -> нет
 */
 
+int number;
+Console.Write("Введите номер дня недели: ");
+int.TryParse(Console.ReadLine()!, out number);
+
+if (number <= 0 || number >= 8)
+    Console.Write("Нет такого дня недели");
+else if (number == 6 || number == 7)
+    Console.Write("Выходной день");
+else if (number >= 1 && number <= 5)
+    Console.Write("Будни");
