43342fe49a768ac115696e73e563f1e5c439d37b
GenaPC <lotog@yandex.ru>
2023-02-19 13:37:15 +0300

    Решение задачи на ветке task6

--- Program.cs
+++ Program.cs
@@ -6,3 +6,21 @@
 32679 -> 6
 */
 
+int N;
+Console.Write("Введите число: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+
+if (flag)
+{
+    if (N > 100 && N < 999)
+        Console.Write($"{N} -> {N / 1 % 10}");
+    else if (N > 1000 && N < 9999)
+        Console.Write($"{N} -> {N / 10 % 10}");
+    else if (N > 10000 && N < 99999)
+        Console.Write($"{N} -> {N / 100 % 10}");
+    else if (N > 100000)
+        Console.Write($"{N} Большое число");
+    else Console.Write("Третьей цифры нет");
+}
+else
+    Console.Write("Неверный ввод");
