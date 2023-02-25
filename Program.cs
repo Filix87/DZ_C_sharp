d37ccd39e83403d1d955f1d9a1bc9c237c9b5659
GenaPC <lotog@yandex.ru>
2023-02-25 23:16:11 +0300

    решение задачи 27 на ветке task12

--- Program.cs
+++ Program.cs
@@ -4,3 +4,22 @@
 82 -> 10
 9012 -> 12                            */
 
+int a = TakeInt();
+Console.Write($"{a} -> {Sum(a)}");
+int Sum(int _a)
+{
+    int sum = 0;
+    for (int i = 3; i >= 0; i--)
+    {
+        sum += a / (int)Math.Pow(10, i);
+        a = a % (int)Math.Pow(10, i);
+    }
+    return sum;
+}
+int TakeInt(string _str = "Введите число: ")
+{
+    int _num;
+    Console.Write(_str);
+    int.TryParse(Console.ReadLine()!, out _num);
+    return _num;
+}
