f68d38b1b1d162ce3d1d0f55054f71c9ab29d760
GenaPC <lotog@yandex.ru>
2023-02-25 21:21:43 +0300

    Решение задачи на ветке task11

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16                              */
 
+double a = 1;
+double b = 1;
+double step = 0;
+
+Console.Write($"Введите число ");
+double.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите степень ");
+double.TryParse(Console.ReadLine()!, out b);
+
+if (a != 0 && b != 0)
+    Console.Write($"{step = Math.Pow(a, b)}");
+else Console.Write($"Неверный ввод");
