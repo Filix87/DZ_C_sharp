e93b46bcfb8b0cc336c2cf088dcd314c4b2db030
GenaPC <lotog@yandex.ru>
2023-02-25 13:43:00 +0300

    new commit

--- Program.cs
+++ Program.cs
@@ -5,27 +5,21 @@ A (7,-5, 0); B (1,-1,9) -> 11.53                                                
 
 double[] A = new double[3];
 double[] B = new double[3];
-
 Console.Write($"Введите координату х точки А: ");
 double.TryParse(Console.ReadLine()!, out A[0]);
 Console.Write($"Введите координату y точки А: ");
 double.TryParse(Console.ReadLine()!, out A[1]);
 Console.Write($"Введите координату z точки А: ");
 double.TryParse(Console.ReadLine()!, out A[2]);
-
 Console.Write($"Введите координату х точки B: ");
 double.TryParse(Console.ReadLine()!, out B[0]);
 Console.Write($"Введите координату y точки B: ");
 double.TryParse(Console.ReadLine()!, out B[1]);
 Console.Write($"Введите координату z точки B: ");
 double.TryParse(Console.ReadLine()!, out B[2]);
-
 double Xdist = Math.Pow(A[0] - B[0], 2);
 double Ydist = Math.Pow(A[1] - B[1], 2);
 double Zdist = Math.Pow(A[2] - B[2], 2);
-
 double ans = Math.Sqrt(Xdist + Ydist + Zdist);
-
 ans = Math.Round(ans, 2);
-
 Console.Write($"A({A[0]};({A[1]};({A[2]}) B({B[0]};({B[1]});({B[2]}) -> {ans}");
