20222b12fd3993b5e0e74be83a51ec54d2360273
GenaPC <lotog@yandex.ru>
2023-02-25 23:23:50 +0300

    решение задачи 29 на ветке task13

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33                             */
 
+int[] CreateArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 99);
+    return array;
+}
