ab936a6ab06b63a68f70b63f9a489c0b2e509e60
GenaPC <lotog@yandex.ru>
2023-02-26 00:17:18 +0300

    решение задачи 29 в ветке task13

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33                             */
 
+int[] array = CreateArray();
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]}, ");
+
 int[] CreateArray()
 {
     int[] array = new int[8];
