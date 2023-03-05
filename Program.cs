ebc2948397bbcec66ab26cc544ba0d0db2a8a880
GenaPC <lotog@yandex.ru>
2023-03-05 06:28:44 +0300

    решение задачи 34 на ветке task14

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 ﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.
-[345, 897, 568, 234] -> 2                                              */
-
+[345, 897, 568, 234] -> 2                                             */
+int[] array = FillArray(12, 100, 999);
+Console.Write($"{String.Join(", ", array)}\n");
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество четных чисел: " + count);
