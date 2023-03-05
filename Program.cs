980833f0ade9b1717b09e142e195c7a83b0f5fdd
GenaPC <lotog@yandex.ru>
2023-03-05 07:15:58 +0300

    Решение задачи на 36 на ветке task15

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0                                                    */
 
+int[] array = FillArray(8, -9, 99);
+Console.Write($"{String.Join(", ", array)}");
+int sum = 0;
+for (int i = 1; i < array.Length; i += 2)
+{
+    sum += array[i];
+}
+Console.Write($" -> {sum}");
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
