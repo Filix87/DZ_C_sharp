18faebf4266205c5671f490e1a26cfe1d6174af2
GenaPC <lotog@yandex.ru>
2023-03-10 17:01:54 +0300

    решение задачи 47 на ветке task19

--- Program.cs
+++ Program.cs
@@ -4,3 +4,39 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9                                   */
 
+int m = Prompt("Введите количество строк: ");
+int n = Prompt("Введите количество столбцов: ");
+double[,] array = GetArray(m, n);
+PrintArray(array);
+
+int Prompt(string arr)
+{
+    Console.Write(arr);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+double[,] GetArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10.0;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
