405a01398936ec2456164ba8d7bd194e90feed13
GenaPC <lotog@yandex.ru>
2023-03-13 23:31:29 +0300

    решение задачи 54 на ветке task22

--- Program.cs
+++ Program.cs
@@ -9,3 +9,49 @@
 9 5 3 2
 8 4 4 2                               */
 
+
+int m = el("Введите количество строк: ");
+int n = el("Введите количество столбцов: ");
+int[,] array = new int[m, n];
+int el(string arr)
+{
+    Console.Write(arr);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+Console.WriteLine();
+Console.WriteLine("Получился такой массив:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, k] > array[i, j])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив по убыванию элументы в каждой строке:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
