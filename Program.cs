817defa665ae7ca449cf9d4d5a707518bb8dc375
GenaPC <lotog@yandex.ru>
2023-03-16 01:10:25 +0300

    решение задачи 58 на ветке task24

--- Program.cs
+++ Program.cs
@@ -5,4 +5,44 @@
 Результирующая матрица будет:
 18 20
 15 18                        */
+int[,] a = new int[2, 2];
+int[,] b = new int[2, 2];
+for (int i = 0; i < a.GetLength(0); i++)
+    for (int j = 0; j < a.GetLength(1); j++)
+        a[i, j] = new Random().Next(10);
+for (int i = 0; i < b.GetLength(0); i++)
+    for (int j = 0; j < b.GetLength(1); j++)
+        b[i, j] = new Random().Next(10);
 
+if (a.GetLength(1) != b.GetLength(0))
+{
+    Console.Write($"Матрицы нельзя умножить");
+    return;
+}
+int[,] c = new int[a.GetLength(0), b.GetLength(1)];
+
+for (int i = 0; i < c.GetLength(0); i++)
+    for (int j = 0; j < c.GetLength(1); j++)
+        for (int k = 0; k < a.GetLength(1); k++)
+            c[i, j] += a[i, k] * b[k, j];
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+        Console.Write($"   {a[i, j]}");
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < b.GetLength(1); j++)
+        Console.Write($"   {b[i, j]}");
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < c.GetLength(0); i++)
+{
+    for (int j = 0; j < c.GetLength(1); j++)
+        Console.Write(String.Format(" {0,4}", c[i, j]));
+    Console.WriteLine();
+}
