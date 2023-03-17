06522ca20443dd96b22ac80a3d9730e838123f26
GenaPC <lotog@yandex.ru>
2023-03-17 22:46:20 +0300

    решение задачи 66 на ветке task28

--- Program.cs
+++ Program.cs
@@ -3,3 +3,19 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30                     */
 
+int m;
+int n;
+Console.Write($"Введите число M: ");
+int.TryParse(Console.ReadLine(), out m);
+
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write($"M = {m}; N = {n} -> {PrintNumbers(m, n)}");
+
+int PrintNumbers(int m, int n)
+{
+    if (n == m)
+        return n;
+    return n + PrintNumbers(m, n - 1);
+}
