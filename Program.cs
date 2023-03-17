9438bda140d138b763561aca0fe0fc973e8646c6
GenaPC <lotog@yandex.ru>
2023-03-17 23:00:33 +0300

    решение задачи 68 на ветке task29

--- Program.cs
+++ Program.cs
@@ -3,3 +3,19 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29                   */
 
+Console.Write("Введите значение m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Akkerman(m, n)}");
+
+static int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
