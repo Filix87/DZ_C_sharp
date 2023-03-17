64ed160b01412cb859cec8b4b8dfdf8b493bb6dc
GenaPC <lotog@yandex.ru>
2023-03-17 22:05:11 +0300

    решение задачи 64 на ветке task27

--- Program.cs
+++ Program.cs
@@ -3,3 +3,23 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1                     */
 
+#pragma warning disable
+using System;
+int m = 1;
+int n = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write($"N = {n} -> ");
+
+Console.WriteLine($"{PrintNumbers(m, n)}");
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    string result = PrintNumbers(start + 1, end) + ", " + start.ToString();
+    return result;
+}
